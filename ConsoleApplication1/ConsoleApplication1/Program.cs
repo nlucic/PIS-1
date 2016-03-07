71421c338385114e2b070a494b3acd0d1b9390ed
mlendi00 <mlendi00@fesb.hr>
2016-03-07 09:01:27 +0100

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,8 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            Console.WriteLine("Hello World!");
         }
     }
 }
